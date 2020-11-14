f43e9e0f46fa373693cae1070c6484548cc19761
Aaron32365 <aaronsphone32365@gmail.com>
2020-11-14 05:45:45 +0000

    added objectives to complete, added func to find empty index on board

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,11 @@
 # sudoku board
+
+# Goals:
+# 1. loop through board, find an empty space (index with 0 value)
+# 2. try every number until number that will satisfy sudoku game rules is found
+# 3. repeat step 1 and 2 until unable space that is unable to satisfy game rules are found
+# 4. use backtrack algorithm to fix previous mistakes until last empty space is able to satisfy game rules 
+
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -26,6 +33,15 @@ def print_board(bo):
             if j == 8:
                 print(bo[i][j])
             else:
+                # dont newline after print
                 print(str(bo[i][j]) + " ", end="")
 
 print_board(board)
+
+
+def find_empty(bo):
+    # loop through board, find first empty space
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i,j) # i = row, j = column aka (y,x)
